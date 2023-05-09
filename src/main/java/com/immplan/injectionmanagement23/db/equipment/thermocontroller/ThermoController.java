f8d7923f6e2609d60b538304e3fee8825e651b0b
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-09 13:34:21 +0300

    added @Entity because of repositories

--- src/main/java/com/immplan/injectionmanagement23/db/equipment/thermocontroller/ThermoController.java
+++ src/main/java/com/immplan/injectionmanagement23/db/equipment/thermocontroller/ThermoController.java
@@ -2,6 +2,10 @@ package com.immplan.injectionmanagement23.db.equipment.thermocontroller;
 
 
 import com.immplan.injectionmanagement23.db.equipment.Equipment;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "thermo_controller")
 public class ThermoController extends Equipment {
 }
