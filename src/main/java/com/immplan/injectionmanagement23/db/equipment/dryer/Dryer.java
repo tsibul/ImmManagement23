742745ff41338d8c9a92eb1d6b57cf452b52e70e
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-09 13:31:44 +0300

    added @Entity because of repositories

--- src/main/java/com/immplan/injectionmanagement23/db/equipment/dryer/Dryer.java
+++ src/main/java/com/immplan/injectionmanagement23/db/equipment/dryer/Dryer.java
@@ -1,7 +1,10 @@
 package com.immplan.injectionmanagement23.db.equipment.dryer;
 
-
 import com.immplan.injectionmanagement23.db.equipment.Equipment;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "dryer")
 public class Dryer extends Equipment {
 }
--- src/main/java/com/immplan/injectionmanagement23/db/equipment/grinder/Grinder.java
+++ src/main/java/com/immplan/injectionmanagement23/db/equipment/grinder/Grinder.java
@@ -2,6 +2,10 @@ package com.immplan.injectionmanagement23.db.equipment.grinder;
 
 
 import com.immplan.injectionmanagement23.db.equipment.Equipment;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "grinder")
 public class Grinder extends Equipment {
 }
