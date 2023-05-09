d3508991e4426c1d28b2cce609c0eaff55675b9a
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-09 13:28:28 +0300

    tring to start server

--- src/main/java/com/immplan/injectionmanagement23/InjectionManagement23Application.java
+++ src/main/java/com/immplan/injectionmanagement23/InjectionManagement23Application.java
@@ -1,5 +1,6 @@
 package com.immplan.injectionmanagement23;
 
+import com.immplan.injectionmanagement23.controller.MainPageController;
 import com.immplan.injectionmanagement23.db.equipment.EquipmentTypeRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,16 +13,20 @@ public class InjectionManagement23Application {
 
     public static void main(String[] args) {
         SpringApplication.run(InjectionManagement23Application.class, args);
+        //SpringApplication.run(MainPageController.class, args);
     }
 
 
+/*
     @Bean
     public CommandLineRunner run(EquipmentTypeRepository query) {
         return (args) -> {
+
             System.out.println("start");
             System.out.println(query.findById(4L));
             System.out.println("end");
+
         };
     }
-
+*/
 }
