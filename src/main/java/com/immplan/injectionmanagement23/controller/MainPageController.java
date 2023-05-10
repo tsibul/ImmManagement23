c46237cac18266809e3efd036df7058f4363869d
Kate Belozertseva <katebelozertseva@gmail.com>
2023-05-10 20:22:17 +0300

    Added Config-file, Initializer. Installed Thymeleaf and DispatcherServlet.

--- src/main/java/com/immplan/injectionmanagement23/controller/MainPageController.java
+++ src/main/java/com/immplan/injectionmanagement23/controller/MainPageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainPageController {
-        @GetMapping("/")
+        @GetMapping("/index")
         @ResponseBody
         public String myPage() {
             return "index";
