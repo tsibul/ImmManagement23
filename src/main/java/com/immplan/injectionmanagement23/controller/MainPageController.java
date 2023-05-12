942e8b722779211f537ec05e108701fce41504a3
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-12 17:38:34 +0300

    Prepare static pages

--- src/main/java/com/immplan/injectionmanagement23/controller/MainPageController.java
+++ src/main/java/com/immplan/injectionmanagement23/controller/MainPageController.java
@@ -6,8 +6,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainPageController {
-        @GetMapping("/")
-        public String myPage() {
-            return "index";
-        }
+    @GetMapping("/")
+    public String mainPage() {
+        return "index";
+    }
+
+    @GetMapping("/techcard")
+    public String techCard() {
+        return "techcard";
+    }
+
+
+    @GetMapping("/products")
+    public String productsPage() {
+        return "products";
+    }
+
+    @GetMapping("/equipment")
+    public String equipmentPage() {
+        return "equipment";
+    }
+
+    @GetMapping("/colors")
+    public String colorsPage() {
+        return "colors";
+    }
+
 }
