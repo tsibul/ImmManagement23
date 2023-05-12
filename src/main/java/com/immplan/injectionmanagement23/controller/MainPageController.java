a9552d7bf5491ec95c77985046d09c27b3a66d61
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-12 16:11:12 +0300

    shows html instead of string

--- src/main/java/com/immplan/injectionmanagement23/controller/MainPageController.java
+++ src/main/java/com/immplan/injectionmanagement23/controller/MainPageController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainPageController {
         @GetMapping("/")
-        @ResponseBody
         public String myPage() {
             return "index";
         }
