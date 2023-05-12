a10f4292003b7fe1a6f78d5df21c8dbef4739680
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-12 16:14:49 +0300

    initial commit

--- src/main/java/com/immplan/injectionmanagement23/controller/MainPageController.java
+++ src/main/java/com/immplan/injectionmanagement23/controller/MainPageController.java
@@ -3,7 +3,6 @@ package com.immplan.injectionmanagement23.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainPageController {
