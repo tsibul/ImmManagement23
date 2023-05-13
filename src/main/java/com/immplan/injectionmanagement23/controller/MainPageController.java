677017b726145c40492cd6a6a55e5a961d30deaa
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-13 23:30:04 +0300

    added model to send data to server

--- src/main/java/com/immplan/injectionmanagement23/controller/MainPageController.java
+++ src/main/java/com/immplan/injectionmanagement23/controller/MainPageController.java
@@ -1,11 +1,18 @@
 package com.immplan.injectionmanagement23.controller;
 
 
+import com.immplan.injectionmanagement23.db.equipment.injectionmoldingmachine.InjectionMoldingMachine;
+import com.immplan.injectionmanagement23.db.equipment.injectionmoldingmachine.repository.InjectionMoldingMachineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainPageController {
+
     @GetMapping("/")
     public String mainPage() {
         return "index";
@@ -22,8 +29,12 @@ public class MainPageController {
         return "products";
     }
 
+    @Autowired
+    private InjectionMoldingMachineRepository injectionMoldingMachineRepository;
     @GetMapping("/equipment")
-    public String equipmentPage() {
+    public String equipmentPage(Model model) {
+        List<InjectionMoldingMachine> immList = injectionMoldingMachineRepository.findAll();
+        model.addAttribute("immList", immList);
         return "equipment";
     }
 
