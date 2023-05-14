b245cac6ddfc627c5358691792a5b32a5a23ea38
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-14 08:42:38 +0300

    added queries to database

--- src/main/java/com/immplan/injectionmanagement23/controller/MainPageController.java
+++ src/main/java/com/immplan/injectionmanagement23/controller/MainPageController.java
@@ -1,8 +1,13 @@
 package com.immplan.injectionmanagement23.controller;
 
 
+import com.immplan.injectionmanagement23.db.equipment.EquipmentType;
+import com.immplan.injectionmanagement23.db.equipment.EquipmentTypeRepository;
 import com.immplan.injectionmanagement23.db.equipment.injectionmoldingmachine.InjectionMoldingMachine;
 import com.immplan.injectionmanagement23.db.equipment.injectionmoldingmachine.repository.InjectionMoldingMachineRepository;
+import com.immplan.injectionmanagement23.db.product.color.ColorGroup;
+import com.immplan.injectionmanagement23.db.product.color.repository.ColorGroupRepository;
+import com.immplan.injectionmanagement23.db.product.color.repository.ColorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,33 +18,59 @@ import java.util.List;
 @Controller
 public class MainPageController {
 
+    @Autowired
+    private InjectionMoldingMachineRepository injectionMoldingMachineRepository;
+    @Autowired
+    private EquipmentTypeRepository equipmentTypeRepository;
+    @Autowired
+    private ColorGroupRepository colorGroupRepository;
+    @Autowired
+    private ColorRepository colorRepository;
+
     @GetMapping("/")
-    public String mainPage() {
+    public String mainPage(Model model) {
+        List<EquipmentType> equipmentTypeList = equipmentTypeRepository.findAll();
+        List<ColorGroup> colorGroupList = colorGroupRepository.findAll();
+        model.addAttribute("colorGroups", colorGroupList);
+        model.addAttribute("equipmentTypes", equipmentTypeList);
         return "index";
     }
 
     @GetMapping("/techcard")
-    public String techCard() {
+    public String techCard(Model model) {
+        List<EquipmentType> equipmentTypeList = equipmentTypeRepository.findAll();
+        List<ColorGroup> colorGroupList = colorGroupRepository.findAll();
+        model.addAttribute("colorGroups", colorGroupList);
+        model.addAttribute("equipmentTypes", equipmentTypeList);
         return "techcard";
     }
 
 
     @GetMapping("/products")
-    public String productsPage() {
+    public String productsPage(Model model) {
+        List<EquipmentType> equipmentTypeList = equipmentTypeRepository.findAll();
+        List<ColorGroup> colorGroupList = colorGroupRepository.findAll();
+        model.addAttribute("colorGroups", colorGroupList);
+        model.addAttribute("equipmentTypes", equipmentTypeList);
         return "products";
     }
-
-    @Autowired
-    private InjectionMoldingMachineRepository injectionMoldingMachineRepository;
     @GetMapping("/equipment")
     public String equipmentPage(Model model) {
         List<InjectionMoldingMachine> immList = injectionMoldingMachineRepository.findAll();
         model.addAttribute("immList", immList);
+        List<EquipmentType> equipmentTypeList = equipmentTypeRepository.findAll();
+        List<ColorGroup> colorGroupList = colorGroupRepository.findAll();
+        model.addAttribute("colorGroups", colorGroupList);
+        model.addAttribute("equipmentTypes", equipmentTypeList);
         return "equipment";
     }
 
     @GetMapping("/colors")
-    public String colorsPage() {
+    public String colorsPage (Model model) {
+        List<EquipmentType> equipmentTypeList = equipmentTypeRepository.findAll();
+        List<ColorGroup> colorGroupList = colorGroupRepository.findAll();
+        model.addAttribute("colorGroups", colorGroupList);
+        model.addAttribute("equipmentTypes", equipmentTypeList);
         return "colors";
     }
 
