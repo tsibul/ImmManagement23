7c52d9136f43e2638c00d2783f574f33d1287baf
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-14 08:44:05 +0300

    query color by color_group

--- src/main/java/com/immplan/injectionmanagement23/db/product/color/repository/ColorRepository.java
+++ src/main/java/com/immplan/injectionmanagement23/db/product/color/repository/ColorRepository.java
@@ -1,7 +1,12 @@
 package com.immplan.injectionmanagement23.db.product.color.repository;
 
 import com.immplan.injectionmanagement23.db.product.color.Color;
+import com.immplan.injectionmanagement23.db.product.color.ColorGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ColorRepository extends JpaRepository<Color, Long> {
+
+    public List<Color> findColorByColorGroup(ColorGroup colorGroup);
 }
