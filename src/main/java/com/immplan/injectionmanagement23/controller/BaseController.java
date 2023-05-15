9fbbd849b3a291d59e4fd8c8b5cef7fd329bb0b8
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-16 00:29:22 +0300

    abstract class to manage menu controller

---
+++ src/main/java/com/immplan/injectionmanagement23/controller/BaseController.java
@@ -0,0 +1,27 @@
+package com.immplan.injectionmanagement23.controller;
+
+import com.immplan.injectionmanagement23.db.equipment.EquipmentType;
+import com.immplan.injectionmanagement23.db.equipment.EquipmentTypeRepository;
+import com.immplan.injectionmanagement23.db.product.color.ColorGroup;
+import com.immplan.injectionmanagement23.db.product.color.repository.ColorGroupRepository;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+public abstract class BaseController {
+    protected final EquipmentTypeRepository equipmentTypeRepository;
+    protected final ColorGroupRepository colorGroupRepository;
+
+    public BaseController(EquipmentTypeRepository equipmentTypeRepository,
+                          ColorGroupRepository colorGroupRepository) {
+        this.equipmentTypeRepository = equipmentTypeRepository;
+        this.colorGroupRepository = colorGroupRepository;
+    }
+
+    protected void populateModel(Model model) {
+        List<EquipmentType> equipmentTypeList = equipmentTypeRepository.findAll();
+        List<ColorGroup> colorGroupList = colorGroupRepository.findAll();
+        model.addAttribute("colorGroups", colorGroupList);
+        model.addAttribute("equipmentTypes", equipmentTypeList);
+    }
+}
