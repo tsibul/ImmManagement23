44e8489a89a3d83bbc054558d64cdbd8c47fcd7c
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-20 22:37:13 +0300

    Deleted as this information planned to be in production report

--- src/main/java/com/immplan/injectionmanagement23/db/product/detail/repository/DetailToProduceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.immplan.injectionmanagement23.db.product.detail.repository;
-
-import com.immplan.injectionmanagement23.db.product.detail.DetailToProduce;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetailToProduceRepository extends JpaRepository<DetailToProduce, Long> {
-}
