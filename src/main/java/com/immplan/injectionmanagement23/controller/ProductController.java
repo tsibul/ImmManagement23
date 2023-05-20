26dd2d7d00b43c881b2ba3f48645fdc953ec0778
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-21 00:36:52 +0300

    import productGroup added new fields for get request

--- src/main/java/com/immplan/injectionmanagement23/controller/ProductController.java
+++ src/main/java/com/immplan/injectionmanagement23/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.immplan.injectionmanagement23.controller;
 
 import com.immplan.injectionmanagement23.db.equipment.EquipmentTypeRepository;
 import com.immplan.injectionmanagement23.db.product.product.Product;
+import com.immplan.injectionmanagement23.db.product.product.ProductGroup;
+import com.immplan.injectionmanagement23.db.product.product.repository.ProductGroupRepository;
 import com.immplan.injectionmanagement23.db.product.product.repository.ProductRepository;
 import com.immplan.injectionmanagement23.db.product.color.repository.ColorGroupRepository;
 import org.springframework.stereotype.Controller;
@@ -15,20 +17,26 @@ import java.util.List;
 public class ProductController extends BaseController {
 
     private final ProductRepository productRepository;
+    private final ProductGroupRepository productGroupRepository;
 
     public ProductController(EquipmentTypeRepository equipmentTypeRepository,
-                             ColorGroupRepository colorGroupRepository, ProductRepository productRepository) {
+                             ColorGroupRepository colorGroupRepository, ProductRepository productRepository,
+                             ProductGroupRepository productGroupRepository) {
         super(equipmentTypeRepository, colorGroupRepository);
 
         this.productRepository = productRepository;
+        this.productGroupRepository = productGroupRepository;
     }
 
     @GetMapping("/products")
     public String getProduct(Model model) {
         List<Product> products = productRepository.findProductByProductActiveOrderByProductCode(true);
+        List<ProductGroup> productGroups = productGroupRepository.
+                findProductGroupsByProductGroupActiveOrderByProductGroupName(true);
         populateModel(model);
         model.addAttribute("activePage", "products");
         model.addAttribute("products", products);
+        model.addAttribute("productGroups", productGroups);
         return "products";
     }
 
@@ -39,7 +47,7 @@ public class ProductController extends BaseController {
     }
 
     @GetMapping("/products/{id}/deleteproduct")
-    public String deleteProduct( @PathVariable int id) {
+    public String deleteProduct(@PathVariable int id) {
         Product product = productRepository.findProductByProductId(id);
         product.setProductActive(false);
         productRepository.save(product);
