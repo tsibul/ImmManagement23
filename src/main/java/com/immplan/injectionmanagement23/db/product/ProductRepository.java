f8ed6908e9cb01f25d6a3f5f1340853528887e28
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-20 18:10:16 +0300

    select active products, find by id

--- src/main/java/com/immplan/injectionmanagement23/db/product/ProductRepository.java
+++ src/main/java/com/immplan/injectionmanagement23/db/product/ProductRepository.java
@@ -2,5 +2,9 @@ package com.immplan.injectionmanagement23.db.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findProductByProductActiveOrderByProductCode(boolean productActive);
+    Product findProductByProductId(long productId);
 }
