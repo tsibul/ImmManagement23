7ed5d66a7e103d3dacfc7e22d71cce4ab23ed5bf
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-21 00:35:49 +0300

    query for active

--- src/main/java/com/immplan/injectionmanagement23/db/product/product/repository/ProductGroupRepository.java
+++ src/main/java/com/immplan/injectionmanagement23/db/product/product/repository/ProductGroupRepository.java
@@ -3,5 +3,8 @@ package com.immplan.injectionmanagement23.db.product.product.repository;
 import com.immplan.injectionmanagement23.db.product.product.ProductGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductGroupRepository extends JpaRepository<ProductGroup, Long> {
+    List<ProductGroup> findProductGroupsByProductGroupActiveOrderByProductGroupName(boolean active);
 }
