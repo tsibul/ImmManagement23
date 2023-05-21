7166342517657fa3106470e843416529096e9858
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-21 11:42:42 +0300

    Sprad menu for products => products,details,detailsToProducts

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -2,6 +2,20 @@ const colorButton = document.querySelector('.dropdown_button.color_button');
 const colorMenu = document.querySelector('.color_menu');
 const equipmentButton = document.querySelector('.dropdown_button.equipment_button');
 const equipmentMenu = document.querySelector('.equipment_menu');
+const productMenu = document.querySelector('.product_menu');
+const productButton = document.querySelector('.dropdown_button.product_button');
+
+productButton.addEventListener('mouseover', function () {
+    productMenu.style.display = 'block';
+});
+
+productButton.addEventListener('mouseout', function () {
+    productMenu.style.display = 'none';
+});
+
+productMenu.addEventListener('mouseout', function () {
+    productMenu.style.display = 'none';
+});
 
 colorButton.addEventListener('mouseover', function () {
     colorMenu.style.display = 'block';
