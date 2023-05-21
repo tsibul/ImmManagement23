d6d874f1fc0fb2263f708d1cc8eef003de1ffff7
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-21 16:27:51 +0300

    reduce code for event listeners

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,42 +1,8 @@
-const colorButton = document.querySelector('.dropdown_button.color_button');
-const colorMenu = document.querySelector('.color_menu');
-const equipmentButton = document.querySelector('.dropdown_button.equipment_button');
-const equipmentMenu = document.querySelector('.equipment_menu');
-const productMenu = document.querySelector('.product_menu');
-const productButton = document.querySelector('.dropdown_button.product_button');
-
-productButton.addEventListener('mouseover', function () {
-    productMenu.style.display = 'block';
-});
-
-productButton.addEventListener('mouseout', function () {
-    productMenu.style.display = 'none';
-});
-
-productMenu.addEventListener('mouseout', function () {
-    productMenu.style.display = 'none';
-});
-
-colorButton.addEventListener('mouseover', function () {
-    colorMenu.style.display = 'block';
-});
-
-colorButton.addEventListener('mouseout', function () {
-    colorMenu.style.display = 'none';
-});
-
-colorMenu.addEventListener('mouseout', function () {
-    colorMenu.style.display = 'none';
-});
-
-equipmentButton.addEventListener('mouseover', function () {
-    equipmentMenu.style.display = 'block';
-});
-
-equipmentButton.addEventListener('mouseout', function () {
-    equipmentMenu.style.display = 'none';
-});
-
-equipmentMenu.addEventListener('mouseout', function () {
-    equipmentMenu.style.display = 'none';
-});
+let menuButtons = document.querySelectorAll('.menu-button');
+menuButtons.forEach(menuListen);
+function menuListen (item){
+    item.addEventListener('mouseover', function (){
+    document.getElementById(item.dataset.menu).style.display = 'block';});
+    item.addEventListener('mouseout', function () {
+        document.getElementById(item.dataset.menu).style.display = 'none';});
+}
