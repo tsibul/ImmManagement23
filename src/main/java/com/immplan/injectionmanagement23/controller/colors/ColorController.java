a8224691aeff6a7fac1fc052f7fe0cbba8c02521
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-22 23:20:59 +0300

    not used colorId param

--- src/main/java/com/immplan/injectionmanagement23/controller/colors/ColorController.java
+++ src/main/java/com/immplan/injectionmanagement23/controller/colors/ColorController.java
@@ -2,7 +2,6 @@ package com.immplan.injectionmanagement23.controller.colors;
 
 import com.immplan.injectionmanagement23.controller.BaseController;
 import com.immplan.injectionmanagement23.db.equipment.EquipmentTypeRepository;
-import com.immplan.injectionmanagement23.db.equipment.injectionmoldingmachine.repository.InjectionMoldingMachineRepository;
 import com.immplan.injectionmanagement23.db.product.color.Color;
 import com.immplan.injectionmanagement23.db.product.color.ColorGroup;
 import com.immplan.injectionmanagement23.db.product.color.repository.ColorGroupRepository;
@@ -47,7 +46,7 @@ public class ColorController extends BaseController {
     }
 
     @PostMapping("/colors/{id}/addcolor")
-    public String addColor(@PathVariable("id") int id, @ModelAttribute Color color, @RequestParam("colorId") int colorId) {
+    public String addColor(@PathVariable("id") int id, @ModelAttribute Color color) {
         colorRepository.save(color);
         return "redirect:/colors/" + id;
     }
