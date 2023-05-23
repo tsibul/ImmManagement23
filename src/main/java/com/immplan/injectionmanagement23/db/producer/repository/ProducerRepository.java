070ad93d2bd04f5433683585ecbefd887b7dd3be
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-23 21:20:54 +0300

    standard queries

--- src/main/java/com/immplan/injectionmanagement23/db/producer/repository/ProducerRepository.java
+++ src/main/java/com/immplan/injectionmanagement23/db/producer/repository/ProducerRepository.java
@@ -3,5 +3,9 @@ package com.immplan.injectionmanagement23.db.producer.repository;
 import com.immplan.injectionmanagement23.db.producer.Producer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProducerRepository extends JpaRepository<Producer, Long> {
+    List<Producer> findProducerByProducerActiveOrderByProducerName(boolean producerActive);
+    Producer findProducerById(Long id);
 }
