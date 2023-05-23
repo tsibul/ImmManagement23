3fd49d63ff949ec341e7749734c5adbc1b113107
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-23 09:28:39 +0300

    set default 0

--- src/main/java/com/immplan/injectionmanagement23/db/rawmaterial/MaterialType.java
+++ src/main/java/com/immplan/injectionmanagement23/db/rawmaterial/MaterialType.java
@@ -7,13 +7,13 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "material_type")
-public class MaterialType extends Material{
+public class MaterialType extends Material {
     @Column(name = "drying")
     private boolean drying;
-    @Column(name = "drying_time")
-    private int materialDryingTime;
-    @Column(name = "driyng_temperature")
-    private int materialDryingTemperature;
+    @Column(name = "drying_time", columnDefinition = "int default 0", nullable = false)
+    private int materialDryingTime = 0;
+    @Column(name = "driyng_temperature", columnDefinition = "int default 0", nullable = false)
+    private int materialDryingTemperature = 0;
 
     public boolean isDrying() {
         return drying;
