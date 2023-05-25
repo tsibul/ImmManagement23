5591fc8f24100e4ed7c09651cc7b992bdb49ea58
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-26 00:02:41 +0300

    standard queries

--- src/main/java/com/immplan/injectionmanagement23/db/rawmaterial/repository/MainMaterialRepository.java
+++ src/main/java/com/immplan/injectionmanagement23/db/rawmaterial/repository/MainMaterialRepository.java
@@ -3,5 +3,16 @@ package com.immplan.injectionmanagement23.db.rawmaterial.repository;
 import com.immplan.injectionmanagement23.db.rawmaterial.MainMaterial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MainMaterialRepository extends JpaRepository<MainMaterial, Long> {
+    MainMaterial findMainMaterialByMaterialId(Long materialId);
+    List<MainMaterial> findMainMaterialByMaterialActiveOrderByMaterialName(boolean mainMaterialActive);
+    List<MainMaterial> findMainMaterialByMaterialActiveAndMaterialTypeMaterialIdOrderByMaterialName
+            (boolean mainMaterialActive, Long materialTypeId);
+    List<MainMaterial> findMainMaterialByMaterialActiveAndProducerIdOrderByMaterialName
+            (boolean mainMaterialActive, Long producerId);
+    List<MainMaterial> findMainMaterialByMaterialActiveAndProducerIdAndMaterialTypeMaterialIdOrderByMaterialName
+            (boolean mainMaterialActive, Long producerId, Long materialTypeId);
+
 }
