b25102eebf7620514f457e07c3b35c09853f1134
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-28 09:08:08 +0300

    separate modal logic to universal for materialtypes.js

--- src/main/resources/static/js/materials/materialtypes.js
+++ src/main/resources/static/js/materials/materialtypes.js
@@ -1,3 +1,44 @@
+// Импортируем функции из main_modal.js
+import initialize from '/js/main_modal.js';
+
+function inputs(modal) {
+    return {
+        materialTypeId: modal.querySelector("#material-type-id"),
+        materialTypeName: modal.querySelector("#material-type-name"),
+        drying: modal.querySelector("#material-type-drying"),
+        materialDryingTime: modal.querySelector("#material-drying-time"),
+        materialDryingTemperature: modal.querySelector("#material-drying-temperature")
+    };
+}
+
+function fullData(row) {
+    return {
+        materialTypeId: row.dataset.materialtypeid,
+        materialTypeName: row.querySelector("[data-label='название']").textContent,
+        drying: row.querySelector("[data-label='сушка']").dataset.drying,
+        materialDryingTime: row.querySelector("[data-label='время сушки (ч)']").textContent,
+        materialDryingTemperature: row.querySelector("[data-label='температура сушки']").textContent
+    };
+}
+
+function emptyData() {
+    return {
+        materialTypeId: 0,
+        drying: true,
+        materialDryingTime: "0",
+        materialDryingTemperature: "0"
+    };
+}
+
+function action (){
+    return "/materialtypes/addmaterial";
+}
+
+// Вызываем функцию initialize, передавая необходимые функции
+initialize(inputs, emptyData, fullData, action);
+
+/*
+
 document.addEventListener("DOMContentLoaded", function() {
     const colorRows = document.querySelectorAll(".color-row");
     const modal = document.querySelector(".modal");
@@ -79,3 +120,5 @@ document.addEventListener("DOMContentLoaded", function() {
         modal.style.display = "none";
     });
 });
+
+ */
