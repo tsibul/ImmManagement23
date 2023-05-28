f6fa2855e02791c99faab496268019b584286eaf
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-28 08:18:25 +0300

    separate modal logic to universal for country

--- src/main/resources/static/js/dicts/country.js
+++ src/main/resources/static/js/dicts/country.js
@@ -1,3 +1,38 @@
+// Импортируем функции из main_modal.js
+import initialize from '/js/main_modal.js';
+
+function inputs(modal) {
+    return {
+        countryId: modal.querySelector("#country-id"),
+        countryName: modal.querySelector("#country-name")
+    };
+}
+
+function fullData(row) {
+    return {
+        countryId: row.dataset.colorgroupid,
+        countryName: row.querySelector("[data-label='название']").textContent
+
+    };
+}
+
+function emptyData() {
+    return {
+        countryId: 0,
+    };
+}
+
+function action (){
+    return "/country/addcountry";
+}
+
+// Вызываем функцию initialize, передавая необходимые функции
+initialize(inputs, emptyData, fullData, action);
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const colorRows = document.querySelectorAll(".color-row");
     const modal = document.querySelector(".modal");
