983b2d0b21213d68212d2941afb1ec6d925f622c
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-28 10:11:06 +0300

    separate main_modal from products.js

--- src/main/resources/static/js/products/products.js
+++ src/main/resources/static/js/products/products.js
@@ -1,3 +1,47 @@
+// Импортируем функции из main_modal.js
+import initialize from '/js/main_modal.js';
+
+const colorGroupsId = document.querySelector("#url-parameters").dataset.colorgroupsid;
+const productGroupsId = document.querySelector("#url-parameters").dataset.productgroupsid;
+
+function inputs(modal) {
+    return {
+        productId: modal.querySelector("#product-id"),
+        productCode: modal.querySelector("#product-code"),
+        productName: modal.querySelector("#product-name"),
+        numberDetails: modal.querySelector("#number-details"),
+        colorGroup: modal.querySelector("#color-group"),
+        productGroup: modal.querySelector("#product-group")
+    };
+}
+
+function fullData(row) {
+    return {
+        productId: row.dataset.productid,
+        productCode: row.querySelector("[data-label='артикул']").textContent,
+        productName: row.querySelector("[data-label='название']").textContent,
+        numberDetails: row.querySelector("[data-label='кол-во деталей']").textContent,
+        colorGroup: row.querySelector("[data-label='группа цветов']").dataset.id,
+        productGroup: row.querySelector("[data-label='группа товара']").dataset.id
+    };
+}
+
+function emptyData() {
+    return {
+        productId: 0,
+        colorGroup: colorGroupsId,
+        productGroup: productGroupsId
+    };
+}
+
+function action() {
+    return "/products/addproduct?colorGroupsId=" + colorGroupsId + "&productGroupsId=" + productGroupsId;
+}
+
+// Вызываем функцию initialize, передавая необходимые функции
+initialize(inputs, emptyData, fullData, action);
+
+/*
 document.addEventListener("DOMContentLoaded", function () {
     const tableRows = document.querySelectorAll(".table-row");
     const modal = document.querySelector("#product-modal");
@@ -86,3 +130,6 @@ document.addEventListener("DOMContentLoaded", function () {
         modal.style.display = "none";
     });
 });
+
+
+ */
