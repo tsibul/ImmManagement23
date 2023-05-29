38269e585ec2a82e35801192f2f70a4a0ca06c16
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-05-29 08:32:17 +0300

    Query for detail controller

--- src/main/java/com/immplan/injectionmanagement23/db/equipment/mold/repository/MoldInsertRepository.java
+++ src/main/java/com/immplan/injectionmanagement23/db/equipment/mold/repository/MoldInsertRepository.java
@@ -3,5 +3,9 @@ package com.immplan.injectionmanagement23.db.equipment.mold.repository;
 import com.immplan.injectionmanagement23.db.equipment.mold.MoldInsert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MoldInsertRepository extends JpaRepository<MoldInsert, Long> {
+    MoldInsert findMoldInsertByEquipmentIdOrderByEquipmentName(Long equipmentId);
+    List<MoldInsert> findMoldInsertByEquipmentActiveOrderByEquipmentName(boolean equipmentActive);
 }
