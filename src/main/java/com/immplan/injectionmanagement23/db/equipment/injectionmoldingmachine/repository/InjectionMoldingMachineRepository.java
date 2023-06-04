bd8f4c5ba86ecae6d49040ce3579b3fcef917704
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-06-05 01:00:38 +0300

    find by Id & find by Active

--- src/main/java/com/immplan/injectionmanagement23/db/equipment/injectionmoldingmachine/repository/InjectionMoldingMachineRepository.java
+++ src/main/java/com/immplan/injectionmanagement23/db/equipment/injectionmoldingmachine/repository/InjectionMoldingMachineRepository.java
@@ -3,5 +3,9 @@ package com.immplan.injectionmanagement23.db.equipment.injectionmoldingmachine.r
 import com.immplan.injectionmanagement23.db.equipment.injectionmoldingmachine.InjectionMoldingMachine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InjectionMoldingMachineRepository extends JpaRepository<InjectionMoldingMachine, Long> {
+    InjectionMoldingMachine findInjectionMoldingMachineByEquipmentId(Long equipmentId);
+    List<InjectionMoldingMachine> findInjectionMoldingMachineByEquipmentActiveOrderByInternalNo(boolean equipmentActive);
 }
