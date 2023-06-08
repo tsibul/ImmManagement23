9a6df66521e9046a3e1d0070349b5dcce7666177
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-06-08 07:13:20 +0300

    make query to match mold to injection molding machine now not used should be for future

--- src/main/java/com/immplan/injectionmanagement23/db/equipment/equipmentmatching/repository/MoldBaseToInjectionMoldingMachineRepository.java
+++ src/main/java/com/immplan/injectionmanagement23/db/equipment/equipmentmatching/repository/MoldBaseToInjectionMoldingMachineRepository.java
@@ -2,6 +2,27 @@ package com.immplan.injectionmanagement23.db.equipment.equipmentmatching.reposit
 
 import com.immplan.injectionmanagement23.db.equipment.equipmentmatching.MoldBaseToInjectionMoldingMachine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MoldBaseToInjectionMoldingMachineRepository extends JpaRepository<MoldBaseToInjectionMoldingMachine, Long> {
+/*    interface EquipmentIdProjection {
+        Long getInjectionMoldingMachineEquipmentId();
+    }
+
+ */
+//    Integer countByMoldBaseToInjectionMoldingMachineActiveAndMoldBaseEquipmentIdAndInjectionMoldingMachineEquipmentId
+//            (boolean active, Long moldBaseId, Long immId);
+    List<MoldBaseToInjectionMoldingMachine> findMoldBaseToInjectionMoldingMachineByMoldBaseToInjectionMoldingMachineActive
+            (boolean  active);
+    //List<MoldBaseToInjectionMoldingMachine>  findMoldBaseToInjectionMoldingMachineByMoldBaseToInjectionMoldingMachineActiveAndMoldBaseEquipmentId(boolean active, Long moldBaseId);
+
+//    List<EquipmentIdProjection>  findMoldBaseToInjectionMoldingMachineByMoldBaseToInjectionMoldingMachineActiveAndMoldBaseEquipmentId(boolean active, Long moldBaseId);
+
+    @Query("SELECT m.injectionMoldingMachine.equipmentId FROM MoldBaseToInjectionMoldingMachine m " +
+            "WHERE m.moldBaseToInjectionMoldingMachineActive = true " +
+            "AND m.moldBase.equipmentId = :moldBaseId")
+    List<Long> findEquipmentIdsByMoldBaseId(@Param("moldBaseId") Long moldBaseId);
 }
