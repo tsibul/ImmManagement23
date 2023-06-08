409f9be8625164771800673ccebf3954c1796d0f
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-06-08 07:15:50 +0300

    live button pushed

--- src/main/resources/static/js/equipment/equipment_extra.js
+++ src/main/resources/static/js/equipment/equipment_extra.js
@@ -1,11 +1,13 @@
-function showBux(){
+function showBux(obj){
     const buxStrings = document.querySelectorAll(".bux");
     buxStrings.forEach(unShowToggle);
+    obj.classList.toggle("button-cancel-pushed")
 }
 
-function showTech(){
+function showTech(obj){
     const techStrings = document.querySelectorAll(".tech");
     techStrings.forEach( unShowToggle);
+    obj.classList.toggle("button-cancel-pushed")
 }
 
 function unShowToggle(node){
