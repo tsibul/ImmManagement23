fc0bcb5719fc4376756d6d0e062bc3b7b5e42a19
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-06-08 09:39:03 +0300

    MoldBase modal

--- src/main/resources/static/js/equipment/mold_base.js
+++ src/main/resources/static/js/equipment/mold_base.js
@@ -4,21 +4,56 @@ import initialize from '/js/main_modal.js';
 
 function inputs(modal) {
     return {
+        equipmentId: modal.querySelector("#mold-base-id"),
+        equipmentName: modal.querySelector("#mold-base-name"),
+        inventoryCode: modal.querySelector("#inventory-code"),
+        equipmentCode: modal.querySelector("#code"),
+        producer: modal.querySelector("#producer"),
+        partNumber: modal.querySelector("#part-number"),
+        yearProduction: modal.querySelector("#year-production"),
+        receiveDate: modal.querySelector("#receive-date"),
+        moldBaseWidthMm: modal.querySelector("#mold-base-width"),
+        moldBaseLengthMm: modal.querySelector("#mold-base-length"),
+        ifChangeableModifier: modal.querySelector("#changeable-modifier"),
+        hotRunner: modal.querySelector("#hot-runner"),
+        movingPlateRingDiameterMm: modal.querySelector("#moving-plate-ring"),
+        fixedPlateRingDiameterMm: modal.querySelector("#fixed-plate-ring"),
+        moldMountTimeMin: modal.querySelector("#mount-time"),
+        moldReleaseTimeMin: modal.querySelector("#release-time")
     };
 }
 
 function fullData(row) {
     return {
+        equipmentId: row.dataset.moldbaseid,
+        equipmentName: row.querySelector("[data-label='название']").textContent,
+        inventoryCode: row.querySelector("[data-label='инв. номер']").textContent,
+        equipmentCode: row.querySelector("[data-label='код']").textContent,
+        producer: row.querySelector("[data-label='производитель']").dataset.id,
+        partNumber: row.querySelector("[data-label='зав. номер']").textContent,
+        yearProduction: row.querySelector("[data-label='год вып.']").textContent,
+        receiveDate: row.querySelector("[data-label='на баланс']").textContent,
+        moldBaseWidthMm: row.querySelector("[data-label='ширина, мм']").textContent,
+        moldBaseLengthMm: row.querySelector("[data-label='длина, мм']").textContent,
+        ifChangeableModifier: row.querySelector("[data-label='сменный модификатор']").dataset.bool,
+        hotRunner: row.querySelector("[data-label='горячий канал']").dataset.bool,
+        movingPlateRingDiameterMm: row.querySelector("[data-label='фланец подв. плита']").textContent,
+        fixedPlateRingDiameterMm: row.querySelector("[data-label='фланец неподв. плита']").textContent,
+        moldMountTimeMin: row.querySelector("[data-label='время монтажа, мин']").textContent,
+        moldReleaseTimeMin: row.querySelector("[data-label='время  демонтажа, мин']").textContent
     };
 }
 
 function emptyData() {
     return {
+        equipmentId: 0,
+        ifChangeableModifier: "true",
+        hotRunner: "false",
     };
 }
 
 function action (){
-    return ;
+    return "/equipment/02.01/add_mold_base";
 }
 
 // Вызываем функцию initialize, передавая необходимые функции
