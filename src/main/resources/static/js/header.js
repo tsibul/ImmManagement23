4277c31bb602030bf7de671207ddfbd1d9907756
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-06-18 12:16:49 +0300

    SubMenu open by click

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,11 +1,38 @@
-/*
-let menuButtons = document.querySelectorAll('.menu-button');
-menuButtons.forEach(menuListen);
-function menuListen (item){
-    item.addEventListener('mouseover', function (){
-    document.getElementById(item.dataset.menu).style.display = 'block';});
-    item.addEventListener('mouseout', function () {
-        document.getElementById(item.dataset.menu).style.display = 'none';});
-}
+const dropDownButtons = document.querySelectorAll(".dropdown_button")
+dropDownButtons.forEach(function (button) {
+    button.addEventListener("mouseenter", () => {
+        const dropDownMenu = button.querySelector(".dropdown_menu")
+        dropDownMenu.style.display = 'block';
+    });
+    button.addEventListener("mouseleave", function () {
+        const dropDownMenu = button.querySelector(".dropdown_menu");
+        dropDownMenu.style.display = 'none';
+    });
+    const dropButtons = button.querySelectorAll(".drop_button");
+    dropButtons.forEach(function (dropButton) {
+        dropButton.addEventListener("click", function (event) {
+            if (!event.target.closest(".sub-menu-button")) {
+                event.preventDefault();
+                let allSubMenuButtons = document.querySelectorAll(".sub-menu");
+                allSubMenuButtons.forEach(function (subMenuItem) {
+                    if (subMenuItem !== dropButton.querySelector(".sub-menu")) subMenuItem.style.display = 'none';
+                });
+                const subMenuButtons = dropButton.querySelector(".sub-menu");
+                subMenuButtons.style.display = subMenuButtons.style.display === 'block' ? 'none' : 'block';
+            }
+        });
+    });
+});
 
- */
+function subMenuButtons(inputObj) {
+    /*
+
+    let allSubMenuButtons = document.querySelectorAll(".sub-menu-item");
+    allSubMenuButtons.forEach(function (subMenuItem){
+        subMenuItem.style.display = 'none';
+    } );
+    const subMenuButtons = inputObj.querySelector(".sub-menu-item");
+    subMenuButtons.style.display = 'block';
+     */
+
+}
